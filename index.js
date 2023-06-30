b3dcd8d12c7bfad85c4b628a4c076e74fff9e61e
Kapil Saraswat <101468624+TheKapilSaraswat@users.noreply.github.com>
2023-06-30 23:19:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let submit=document.querySelector(".submit");
+
+submit.addEventListener('click',()=>{
+
+    var output=document.querySelector(".output");
+    console.log(output);
+    let imageurl=document.querySelector("#img").value;
+    var generatedurl=imageurl;
+   
+
+
+    const data={
+        name: document.getElementById("name").value,
+        img: generatedurl,
+        email: document.getElementById("email").value,
+        websitelink: document.getElementById("websitelink").value,
+        skills: document.getElementById("skills").value
+    };
+    console.log(data);
+
+    let newlist=document.createElement('div');
+    newlist.classList.add('addData');
+    newlist.innerHTML=`
+      <img src="${generatedurl}" alt="image">
+      <div class="NameContainer">${data.name}</div>
+      <a href="mailto:kapil.saraswat981@gmaiil.com">${data.email}</a><br>
+      <a class="website link" href="www.kapilsaraswat.com">${data.websitelink}</a>
+      <div class="SkillContainer">${data.skills}</div>
+    `
+    console.log(newlist);
+    output.appendChild(newlist);
+   
+    let input=document.querySelectorAll('input');
+    input.forEach(input => {
+        input.value = '';
+      });
+
+})
